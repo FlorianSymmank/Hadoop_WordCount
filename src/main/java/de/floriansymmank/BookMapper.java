82e442bb15ec399ae431035c19f2dabe9dbb3c53
FlorianSymmank <floriansymmank@gmx.de>
2024-11-21 16:34:19 +0100

    Load stopwords from distributed cache in BookMapper using JsonUtils

--- src/main/java/de/floriansymmank/BookMapper.java
+++ src/main/java/de/floriansymmank/BookMapper.java
@@ -1,34 +1,45 @@
 package de.floriansymmank;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.json.JSONObject;
 
-public class BookMapper extends Mapper<Object, Text, Text, IntWritable>
-{
+import de.floriansymmank.utils.JsonUtils;
+
+public class BookMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+    private Map<String, List<String>> stopwords;
+
     // Runs once at the beginning of the task
     @Override
     protected void setup(Mapper<Object, Text, Text, IntWritable>.Context context)
             throws IOException, InterruptedException {
         super.setup(context);
-    }   
-    
+
+        // Load the stopwords from the distributed cache
+        JSONObject stopwords_json = JsonUtils.loadJsonFile(context, "stopwords.json");
+        Map<String, List<String>> stopwords = JsonUtils.convertJsonToMap(stopwords_json);
+    }
+
     // Runs once for each key-value pair in the input split
     @Override
     protected void map(Object key, Text value, Mapper<Object, Text, Text, IntWritable>.Context context)
-    throws IOException, InterruptedException {
+            throws IOException, InterruptedException {
         Pattern pattern = Pattern.compile("[\\p{L}|\\d]+");
         Matcher matcher = pattern.matcher(value.toString());
-
+                    
         while (matcher.find()) {
             context.write(new Text(matcher.group()), new IntWritable(1));
         }
     }
-    
+
     // Runs once at the end of the task
     @Override
     protected void cleanup(Mapper<Object, Text, Text, IntWritable>.Context context)
