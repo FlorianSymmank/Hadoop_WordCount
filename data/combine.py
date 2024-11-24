af1f514b2ce3ad08ee420615ec7329ef7708c681
FlorianSymmank <floriansymmank@gmx.de>
2024-11-24 14:07:24 +0100

    Add data combining script and update README with usage instructions

---
+++ data/combine.py
@@ -0,0 +1,33 @@
+import os
+from tqdm import tqdm
+
+# Define the target size in bytes (1 GB)
+TARGET_SIZE = 1 * 1024 * 1024 * 1024
+
+# Get all subdirectories of the current directory
+dirs = [d for d in os.listdir('.') if os.path.isdir(
+    d) and not d.startswith('.') and not d.startswith("Test")]
+
+# Combine all files in each subdirectory until the result file is 1 GB
+for idx, d in enumerate(tqdm(dirs, desc="Processing directories", position=0)):
+    # Exclude the output file
+    files = [f for f in os.listdir(d) if f != f"{d}.txt"]
+    path = os.path.join(d, f"{d}.txt")
+    total_size = 0
+
+    with open(path, 'w', encoding="UTF-8") as outfile:
+        with tqdm(total=TARGET_SIZE, unit='B', unit_scale=True, desc=f"Building {d}.txt", position=1, leave=False) as pbar:
+            while total_size <= TARGET_SIZE:
+                for fname in files:
+                    with open(os.path.join(d, fname), encoding="UTF-8") as infile:
+                        content = infile.read()
+                        encoded_content = content.encode('utf-8')
+                        content_size = len(encoded_content)
+                        total_size += content_size
+                        pbar.update(content_size)
+                        outfile.write(content)
+                        if total_size >= TARGET_SIZE:
+                            break
+                else:
+                    continue
+                break
---
+++ data/readme.md
@@ -0,0 +1,9 @@
+This directory contains the data for the project.
+
+Source: 
+- https://www.dropbox.com/scl/fi/bajoqbxtz69sms9ny2kvh/texts.zip?rlkey=6rl8yaxu58kv319xu1genl930&e=1&dl=0
+
+To combine the data into a single file, 1 GB in size, per language, run the following commands:
+- Create python environment and install the required packages. (`pip install -r requirements.txt`)
+- Run the `combine_data.py` script to combine the data into a single file per language.
+- The combined data is saved in the corresponding language directory as <language>.txt file.
