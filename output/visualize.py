232b976ee095a28e57baf2391780d9600e5e0700
FlorianSymmank <floriansymmank@gmx.de>
2025-01-29 23:15:55 +0100

    fix y axis desc

--- output/visualize.py
+++ output/visualize.py
@@ -305,7 +305,7 @@ def plot_runtimes():
             plt.plot(y, x, label=file.replace('.json', '').replace('stats_', ''))
 
     plt.xlabel('Input Size (GB)')
-    plt.ylabel('Elapsed Time (ms)')
+    plt.ylabel('Elapsed Time (sek)')
     plt.title('Runtimes by Input Size')
     plt.legend()
     plt.tight_layout()
